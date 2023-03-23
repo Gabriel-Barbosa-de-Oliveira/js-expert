76977054bf74a46f0cddc5d6d03f1b304e759279
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-03-23 15:18:30 -0300

    Test com Node Nativo

---
+++ modulo01/aula01-mocks/index.test.js
@@ -0,0 +1,16 @@
+
+const { error } = require("./src/constants");
+const File = require("./src/file");
+const assert = require("assert");
+
+//IFEE 
+; (async () => {
+
+    //variaveis criadas nesse bloco, só são validas durante sua execução 
+    {
+        const filePath = "./mocks/emptyFile-invalid.csv"
+        const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+        const result = File.csvToJSON(filePath)
+        await assert.rejects(result, expected);
+    }
+})()
---
+++ modulo01/aula01-mocks/src/file.js
@@ -0,0 +1,33 @@
+const { readFile } = require("fs/promises")
+const { error } = require("./constants")
+
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: ["id", "name", "profession", "age"]
+}
+
+class File {
+    static async csvToJSON(filePath) {
+        const content = await readFile(filePath, "utf8")
+        const validation = this.isValid(content)
+        if (!validation.valid) throw new Error(validation.error)
+    }
+
+    static isValid(csvString, options = DEFAULT_OPTION) {
+        //Para ver o conteudo do arquivo 
+        //fs.readFileSync("./mocks/threeItems-valid.csv", "utf8")
+        //[0] = headers
+        //[1] = linha 1
+        //...variavel = restante do arquivo
+        const [headers, ...fileWithoutHeader] = csvString.split(/\r?\n/)
+        if (!fileWithoutHeader.length) {
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+        console.log(headers, fileWithoutHeader)
+    }
+}
+
+module.exports = File;
