27734558d5414e399832eb87bc616474967cd424
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-03-27 10:50:58 -0300

    Mocked user api

--- modulo01/aula04-e2e-coverage/src/api.js
+++ modulo01/aula04-e2e-coverage/src/api.js
@@ -1,8 +1,40 @@
 const http = require("http")
+const { once } = require("events")
 
-function handler(request,response) {
+const DEFAULT_USER = {
+    username: "GabrielBarbosa",
+    password: "123"
+}
+
+
+const routes = {
+    "/contact:get": (request, response) => {
+        response.write("contact us page")
+        return response.end();
+    },
+
+    //curl -X POST --data '{"username": "GabrielBarbosa", "password":"123"}'  localhost:3000/login (realizer um post via terminal)
+    "/login:post": async (request, response) => {
+        const user = JSON.parse(await once(request, "data"))
+        const toLower = (text) => text.toLowerCase()
+        if(toLower(user.username) !== toLower(DEFAULT_USER.username) || user.password !== DEFAULT_USER.password){
+            response.writeHead(401)
+            response.end("Logging Failed!")
+            return
+        } 
+        return response.end("ok");
+    },
+    default(request, response) {
+        response.writeHead(404)
+        return response.end("not found")
+    }
+}
+
+function handler(request, response) {
     const { url, method } = request
-    return response.end("ok")
+    const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+    const chosen = routes[routeKey] || routes.default
+    return chosen(request, response)
 }
 
 const app = http.createServer(handler).listen(3000, () => console.log("Running at 3000"))
