f3e22db50eb525b403e8a7511da45e3f5dd3a20f
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-03-27 15:17:03 -0300

    Implementando BDD

--- modulo01/aula06-tdd-project-pt02/src/service/carService.js
+++ modulo01/aula06-tdd-project-pt02/src/service/carService.js
@@ -1,12 +1,12 @@
 const BaseRepository = require("./../repository/base/baseRepository")
 
 class CarService {
-    constructor({cars}){
-        this.carRepository = new BaseRepository({file: cars})
+    constructor({ cars }) {
+        this.carRepository = new BaseRepository({ file: cars })
     }
 
-    test(){
-        return this.carRepository.find()
+    async getAvailableCar(carCategory) {
+        return null
     }
 }
 
--- modulo01/aula06-tdd-project-pt02/test/unitTests/carService.test.js
+++ modulo01/aula06-tdd-project-pt02/test/unitTests/carService.test.js
@@ -1,7 +1,31 @@
-const { describe, it } = require("mocha")
+const { describe, it, before } = require("mocha");
+const CarService = require("./../../src/service/carService");
+const { join } = require("path");
+const carsDatabase = join(__dirname, "./../../database", "cars.json")
+const { expect } = require("chai");
 
+const mocks = {
+    validCarCategory: require("./../mocks/valid-carCategory.json"),
+    validCar: require("./../mocks/valid-car.json"),
+    validCustomer: require("./../mocks/valid-customer.json")
+}
 describe("CarService Suite Tests", () => {
-    it("test !", () => {
-        
+    let carService = {};
+
+    before(() => {
+        carService = new CarService({
+            cars: carsDatabase
+        });
+    })
+
+    it("given a car category it should return an available car", async () => {
+        const car = mocks.validCar
+        const carCategory = Object.create(mocks.validCarCategory) //Object.create -> cria uma instancia imutavel do objeto (Como se fosse um cloneDeep)
+        carCategory.ids = [car.id];
+
+        const result = await carService.getAvailableCar(carCategory);
+        const expected = car;
+
+        expect(result).to.be.deep.equal(expected);
     })
 })
