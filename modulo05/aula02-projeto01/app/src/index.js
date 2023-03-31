3185ac81d2ba3295120d3b362912e81d79964f0a
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-03-31 12:58:13 -0300

    Encontrando String correspondente

--- modulo05/aula02-projeto01/app/src/index.js
+++ modulo05/aula02-projeto01/app/src/index.js
@@ -7,5 +7,5 @@ const pdf = require("pdf-parse")
 ;(async () => {
     const dataBuffer = await readFile(join(__dirname,"./../../../docs/contrato.pdf"))
     const data = await pdf(dataBuffer)
-    console.log(data)
+    console.log(data.text)
 })()
