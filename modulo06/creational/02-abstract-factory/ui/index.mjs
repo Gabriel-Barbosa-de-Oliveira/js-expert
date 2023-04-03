2a92b343a893264e718e5a92b5c6104a26fd3b9b
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-04-03 15:47:10 -0300

    Rendering Table Server

--- modulo06/creational/02-abstract-factory/ui/index.mjs
+++ modulo06/creational/02-abstract-factory/ui/index.mjs
@@ -2,7 +2,7 @@ import { database } from "../shared/data.mjs";
 
 class Application {
     constructor(factory) {
-        this.table = factory.createTable();
+        this.table = factory.createTable()
     }
 
     initialize(database) {
@@ -10,13 +10,11 @@ class Application {
     }
 }
 
+
 ; (async function main() {
-    const path = globalThis.window ? "browser" : "console";
 
-    const { default: ViewFactory } = await import(`./../platforms/${path}/index.mjs`);
-
+    const path = globalThis.window ? 'browser' : 'console'
+    const { default: ViewFactory } = await import(`./../platforms/${path}/index.mjs`)
     const app = new Application(new ViewFactory())
     app.initialize(database)
-
-
 })();
