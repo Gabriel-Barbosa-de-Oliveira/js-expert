30b2d9630d542d0b44a059abed1a1c29c3f7ed66
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-04-10 11:05:41 -0300

    Instanciando Classe pagamento

--- modulo07/behavioral/02-observer/test/index.test.js
+++ modulo07/behavioral/02-observer/test/index.test.js
@@ -1,5 +1,6 @@
 import { expect, describe, test, jest } from "@jest/globals";
 import PaymentSubject from "../src/subjects/paymentSubject.js";
+import Payment from "../src/events/payment.js";
 
 describe("Test Suit for Observer Patter", () => {
     test("#PaymentSubject notify observers", () => {
@@ -15,7 +16,7 @@ describe("Test Suit for Observer Patter", () => {
         expect(observer.update).toBeCalledWith(expected)
 
     })
-    test("#PaymentSubject should not notify unbscribed observers", () =>{
+    test("#PaymentSubject should not notify unbscribed observers", () => {
         const subject = new PaymentSubject()
         const observer = {
             update: jest.fn()
@@ -26,6 +27,20 @@ describe("Test Suit for Observer Patter", () => {
         subject.notify(data)
         expect(observer.update).not.toHaveBeenCalled()
     })
-    test.todo("#PaymentSubject should notify subject after a credit card transaction observers")
+    test("#PaymentSubject should notify subject after a credit card transaction observers", () => {
+        const subject = new PaymentSubject()
+        const payment = new Payment(subject)
+
+        const paymentSubjectNotifierSpy = jest.spyOn(
+            payment.paymentSubject,
+            payment.paymentSubject.notify.name
+        )
+
+        const data = { userName: "gabrielbarbosa", id: Date.now() }
+        payment.creditCard(data)
+
+        expect(paymentSubjectNotifierSpy).toHaveBeenCalledWith(data)
+
+    })
     test.todo("#All should notify subscribers after a credit card payment")
 })
