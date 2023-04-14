994e0c97689585ed4b76cf70fb38fa4cfc3da0fc
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-04-14 12:34:51 -0300

    Mockando dados do json - spy on axios

---
+++ modulo07/structural/05-adapter/src/business/integrations/rickAndMortyUSA.js
@@ -0,0 +1,5 @@
+export default class RickAndMortyUSA {
+    static async getCharactersFromXML() {
+
+    }
+}
---
+++ modulo07/structural/05-adapter/test/integrations/rickAndMortyBRL.test.js
@@ -0,0 +1,16 @@
+import { expect, describe, test, jest, beforeEach } from "@jest/globals"
+import fs from "fs/promises"
+import Character from "../../src/entities/character"
+import RickAndMortyBRL from "../../src/business/integrations/rickAndMortyBRL.js"
+import axios from "axios"
+
+describe("#RickAndMortyBRL", () => {
+    test('#getCharatersJSON should return a list of Character Entity', async () => {
+        const response = JSON.parse(await fs.readFile("./test/mocks/characters.json"))
+        const expected = response.results.map(char => new Character(char))
+        jest.spyOn(axios, "get").mockResolvedValue({ data: response })
+        const result = await RickAndMortyBRL.getCharactersFromJSON()
+        expect(result).toStrictEqual(expected)
+    })
+    test.todo('#getCharatersJSON should return an empty list if the API returns nothing')
+})
