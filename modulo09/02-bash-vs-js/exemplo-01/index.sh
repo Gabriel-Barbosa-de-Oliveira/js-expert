aa2b5acb20e4f7398f1f9d1df0ace29c81f869eb
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-06 19:52:13 -0300

    Printing Folder

--- modulo09/02-bash-vs-js/exemplo-01/index.sh
+++ modulo09/02-bash-vs-js/exemplo-01/index.sh
@@ -1,5 +1,8 @@
 FOLDER_AMOUNT=4
 for index in $(seq 1 $FOLDER_AMOUNT); do
+    #1,2 -> bash01, bash02
+    #3,4 -> shell01, shell02
+    folder=$([ $index -ge 3 ] && echo bash-0$index || echo shell-0$index)
 
-echo $index
+    echo $folder
 done
