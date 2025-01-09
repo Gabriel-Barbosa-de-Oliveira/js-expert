d3cc2f44658934fe8f0a7d9f119221d4b58ac32f
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-09 20:19:15 -0300

    Wrinting Folders

--- modulo09/02-bash-vs-js/exemplo-01/index.sh
+++ modulo09/02-bash-vs-js/exemplo-01/index.sh
@@ -3,6 +3,12 @@ for index in $(seq 1 $FOLDER_AMOUNT); do
     #1,2 -> bash01, bash02
     #3,4 -> shell01, shell02
     folder=$([ $index -ge 3 ] && echo bash-0$index || echo shell-0$index)
+    mkdir -p $folder
 
-    echo $folder
+    cd $(pwd)/$folder
+    npm init -y --scope @gabriel_barbosa --silent > /dev/null
+    cat package.json | jq '{n: .name, v: .version}'
+    cd ..
 done
+
+rm -rf bash* shell*
