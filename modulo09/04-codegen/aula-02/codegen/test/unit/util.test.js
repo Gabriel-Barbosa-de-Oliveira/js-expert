03c311d8b50301fdef92887406716df07d5da098
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-16 12:46:11 -0300

    Aula 02

---
+++ modulo09/04-codegen/aula-02/codegen/test/unit/util.test.js
@@ -0,0 +1,41 @@
+import {
+    expect,
+    describe,
+    test,
+    jest,
+    beforeEach
+} from "@jest/globals"
+import Util from "../../src/util.js"
+
+describe("#Util - String", () => {
+    beforeEach(() => {
+        jest.restoreAllMocks()
+        jest.clearAllMocks()
+    })
+
+    test("#upperCaseFirstLetter should transform first letter to upper case", () => {
+        const data = "hello"
+        const expected = "Hello"
+        const result = Util.upperCaseFirstLetter(data)
+        expect(result).toStrictEqual(expected)
+    })
+    test("#lowerFirstLetter should transform first letter to lower case", () => {
+        const data = "Hello"
+        const expected = "hello"
+        const result = Util.lowerCaseFirstLetter(data)
+        expect(result).toStrictEqual(expected)
+    })
+    test("#lowerFirstLetter given an empty string should return an empty", () => {
+        const data = ""
+        const expected = ""
+        const result = Util.lowerCaseFirstLetter(data)
+        expect(result).toStrictEqual(expected)
+    })
+    test("#upperCaseFirstLetter given an empty string should return an empty", () => { 
+        const data = ""
+        const expected = ""
+        const result = Util.upperCaseFirstLetter(data)
+        expect(result).toStrictEqual(expected)
+    })
+
+})
