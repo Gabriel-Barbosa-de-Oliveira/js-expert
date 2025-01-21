2075a288b16ccfccf1b8d37e94a567ea05502e72
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-21 12:24:27 -0300

    Using Create Read Stream

--- modulo10/aula01/1.native-stream.js
+++ modulo10/aula01/1.native-stream.js
@@ -12,4 +12,21 @@
 //node -e "require('net').createServer(socket => socket.pipe(process.stdout)).listen(1338)"
 
 //terminal 2
-//node -e "process.stdin.pipe(require('net').connect(1338))""
+//node -e "process.stdin.pipe(require('net').connect(1338))"
+
+//Gera arquivo granfde 
+//node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
+
+import http from 'http'
+import { createReadStream, readFileSync } from 'fs'
+http.createServer((req, res) => {
+    // Ma pratica
+    // const file = readFileSync('big.file').toString()
+    // res.write(file)
+    // res.end()
+
+    createReadStream('big.file')
+        .pipe(res)
+}).listen(3000, () => console.log("running at 3000"))
+
+//curl localhost:3000 -o output.txt
