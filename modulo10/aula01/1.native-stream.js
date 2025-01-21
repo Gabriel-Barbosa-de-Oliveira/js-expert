e185e9b9c7c491ee7cdfa6ab5d3286bc58c05ae1
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-21 12:17:15 -0300

    Creating Socket

--- modulo10/aula01/1.native-stream.js
+++ modulo10/aula01/1.native-stream.js
@@ -2,8 +2,14 @@
 //Stdin é a entrada padrão do processo
 //Stdout é a saída padrão do processo
 //ls | grep package | xargs cat | jq .name
-process.stdin.pipe(process.stdout)
-.on("data", msg => console.log('data', msg.toString()))
-.on("error", msg => console.log('error', msg.toString()))
-.on("end", _ => console.log('end'))
-.on("close", _ => console.log('close'))
+// process.stdin.pipe(process.stdout)
+// .on("data", msg => console.log('data', msg.toString()))
+// .on("error", msg => console.log('error', msg.toString()))
+// .on("end", _ => console.log('end'))
+// .on("close", _ => console.log('close'))
+
+//terminal 1
+//node -e "require('net').createServer(socket => socket.pipe(process.stdout)).listen(1338)"
+
+//terminal 2
+//node -e "process.stdin.pipe(require('net').connect(1338))""
