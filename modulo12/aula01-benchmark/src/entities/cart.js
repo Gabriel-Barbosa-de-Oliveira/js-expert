31ce49bdc38ddf97c93a7214d0c95298a5cf4f61
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-23 12:06:36 -0300

    Comparing UUID -> Crypto

--- modulo12/aula01-benchmark/src/entities/cart.js
+++ modulo12/aula01-benchmark/src/entities/cart.js
@@ -1,5 +1,5 @@
 import Product from "./product.js"
-import { v4 as uuid } from 'uuid'
+import { randomUUID as uuid } from 'crypto'
 export default class Cart {
     constructor({ at, products }) {
         this.id = uuid()
