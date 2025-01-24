f63134cb66a641c97445a1f7ae5bdc10e67fad15
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-01-24 15:43:22 -0300

    Child Processes

--- modulo13/02-process-report/src/backgroundTask.js
+++ modulo13/02-process-report/src/backgroundTask.js
@@ -1,3 +1,44 @@
-process.on('message', (msg) => console.log("msg from child", msg.toString()))
+import { createReadStream, write } from "fs";
+import { pipeline } from "stream/promises";
+import { setTimeout } from "timers/promises";
+import csvToJson from "csvtojson"
+import { Writable, Transform } from "stream";
 
-console.log(`I'm ready!! ${process.pid}`)
+const database = process.argv[2];
+
+async function onMessage(msg) {
+    const firsTimeRan = [];
+
+    await pipeline(
+        createReadStream(database),
+        csvToJson(),
+        Transform({
+            transform(chunk, enc, cb) {
+                const data = JSON.parse(chunk)
+                if (data.Name !== msg.Name) return cb()
+                if (firsTimeRan.includes(msg.Name)) {
+
+                    return cb(null, msg.Name)
+                }
+
+                firsTimeRan.push(msg.Name)
+                cb()
+            }
+        }),
+        Writable({
+            write(chunk, enc, cb) {
+                if (!chunk) return cb()
+                process.send(chunk.toString())
+                cb()
+            }
+        })
+    )
+}
+
+process.on('message', onMessage)
+
+// console.log(`I'm ready!! ${process.pid}`, database)
+
+// para falar que o sub processo pode morrer apos inatividade
+await setTimeout(5000)
+process.channel.unref()
--- modulo13/02-process-report/src/index.js
+++ modulo13/02-process-report/src/index.js
@@ -1,13 +1,64 @@
-import { fork } from "child_process"
+import { fork } from "child_process";
+import csvToJson from "csvtojson";
+import { createReadStream } from "fs";
+import { Writable } from "stream";
+import { pipeline } from "stream/promises";
 import { setTimeout } from "timers/promises";
-const file = './data/All_Pokemon2.csv'
-const PROCESS_COUNT = 10;
 
+const database = './data/All_Pokemon.csv'
+const PROCESS_COUNT = 30;
+const replications = []
 const backgroundTaskFile = "./src/backgroundTask.js"
+
 const processes = new Map()
 for (let index = 0; index < PROCESS_COUNT; index++) {
-    const child = fork(backgroundTaskFile, [file])
-    await setTimeout(200)
-    child.send('hello world')
+    const child = fork(backgroundTaskFile, [database])
+    child.on('exit', () => {
+        console.log(`process ${child.pid} exited`)
+        processes.delete(child.pid)
+    })
+
+    child.on('error', () => {
+        console.log(`process ${child.pid} has an error`, error)
+        processes.exit(1)
+    })
+
+    child.on('message', msg => {
+        //work around para multiprocessamento
+        if (replications.includes(msg)) return;
+
+        console.log(`${msg} is replicated!`)
+        replications.push(msg)
+    })
+
     processes.set(child.pid, child)
 }
+
+
+function roundRoubin(array, index = 0) {
+    return function () {
+        if (index >= array.length) index = 0
+
+        return array[index++]
+    }
+
+}
+
+
+
+// Pool de conexões, ou load balancer
+const getProcess = roundRoubin([...processes.values()])
+console.log(`starting with $${processes.size} processes`)
+await setTimeout(100)
+
+await pipeline(
+    createReadStream(database),
+    csvToJson(),
+    Writable({
+        write(chunk, enc, cb) {
+            const chosenProcess = getProcess()
+            chosenProcess.send(JSON.parse(chunk))
+            cb()
+        }
+    })
+)
